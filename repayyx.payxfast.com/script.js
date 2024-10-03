bef8677d76ae425a5ff53e77cb6b2287e763767a
FAST PAY <mstaysab96@gmail.com>
2024-10-03 15:56:31 +0600

    Update script.js

--- repayyx.payxfast.com/script.js
+++ repayyx.payxfast.com/script.js
@@ -8,7 +8,7 @@ function copyAmount() {
 }
 
 function copyUPI() {
-    const upi = 'n-majumder@freecharge';
+    const upi = '8974034614@upi';
     navigator.clipboard.writeText(upi).then(() => {
         showPopup('copyModal', 'UPI copied to clipboard!');
     }).catch(err => {
