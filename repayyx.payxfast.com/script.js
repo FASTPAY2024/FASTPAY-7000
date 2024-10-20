2b5205e144893e1b330041d9e9e4014c825954fe
FAST PAY <mstaysab96@gmail.com>
2024-10-20 07:05:20 +0600

    Add files via upload

---
+++ repayyx.payxfast.com/script.js
@@ -0,0 +1,51 @@
+function copyAmount() {
+    const amount = '₹ 9200';
+    navigator.clipboard.writeText(amount).then(() => {
+        showPopup('copyModal', 'Amount copied to clipboard!');
+    }).catch(err => {
+        showPopup('copyModal', 'Failed to copy amount!');
+    });
+}
+
+function copyUPI() {
+    const upi = 'ramushil276-1@oksbi';
+    navigator.clipboard.writeText(upi).then(() => {
+        showPopup('copyModal', 'UPI copied to clipboard!');
+    }).catch(err => {
+        showPopup('copyModal', 'Failed to copy UPI!');
+    });
+}
+
+function showPopup(modalId, message) {
+    document.getElementById(modalId).style.display = "block";
+    document.getElementById('copyMessage').innerText = message;
+}
+
+function closeModal(modalId) {
+    document.getElementById(modalId).style.display = "none";
+}
+
+document.getElementById('submitUtrButton').onclick = function() {
+    document.getElementById('utrModal').style.display = "block";
+};
+
+document.getElementById('confirmButton').onclick = function() {
+    const utrValue = document.getElementById('utrInput').value;
+    if (utrValue) {
+        alert('UTR submitted: ' + utrValue);
+        closeModal('utrModal');
+    } else {
+        alert('Please enter a UTR number before submitting.');
+    }
+};
+function myFunction (radio) {
+  var x = document.getElementById("rdaacls").value;
+}
+// Close modal when clicking outside
+window.onclick = function(event) {
+    if (event.target == document.getElementById('copyModal')) {
+        closeModal('copyModal');
+    } else if (event.target == document.getElementById('utrModal')) {
+        closeModal('utrModal');
+    }
+};
